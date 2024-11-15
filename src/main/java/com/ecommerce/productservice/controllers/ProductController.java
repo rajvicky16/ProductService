7a087369d0019aa336eccc2a352bea3594dfa90a
rajashanmugam <rajvicky16721@gmail.com>
2024-11-15 13:12:51 +0530

    created other folder structures

---
+++ src/main/java/com/ecommerce/productservice/controllers/ProductController.java
@@ -0,0 +1,5 @@
+package com.ecommerce.productservice.controllers;
+
+public class ProductController {
+
+}
---
+++ src/main/java/com/ecommerce/productservice/services/ProductService.java
@@ -0,0 +1,4 @@
+package com.ecommerce.productservice.services;
+
+public class ProductService {
+}
